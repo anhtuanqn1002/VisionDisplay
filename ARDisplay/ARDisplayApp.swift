7eda5fd978a077f6c4cdacaa4bb2cee7be853957
Tuan Nguyen <>
2024-03-15 15:25:48 +0700

    Window Tabs

---
+++ ARDisplay/ARDisplayApp.swift
@@ -0,0 +1,19 @@
+//
+//  ARDisplayApp.swift
+//  ARDisplay
+//
+//  Created by Tuan Nguyen on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ARDisplayApp: App {
+    @State private var currentStyle: ImmersionStyle = .full
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
